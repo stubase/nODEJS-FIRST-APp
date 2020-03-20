2e93b82ab820bb3314e0b9ec27494e407c1ade96
stubase <107356826+stubase@users.noreply.github.com>
2020-03-20 11:04:47 +0000

    Add class

--- app/src/androidTest/java/com/capstone/hibykes/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/capstone/hibykes/ExampleInstrumentedTest.kt
@@ -6,3 +6,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/test
