c9fa5b9008392e74a3ec9ce2de0af512edca2ebd
stubase <107356826+stubase@users.noreply.github.com>
2020-03-20 07:17:43 +0000

    Updated metadata.

--- app/src/androidTest/java/com/capstone/hibykes/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/capstone/hibykes/ExampleInstrumentedTest.kt
@@ -1,3 +1,8 @@
 package com.capstone.hibykes
 
-import androidx.test.plat
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
