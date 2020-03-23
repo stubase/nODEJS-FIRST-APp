b53e9938e8d462c7f3ad69e5eef8cd22756028e0
stubase <107356826+stubase@users.noreply.github.com>
2020-03-23 13:54:59 +0000

    l10n updates

--- app/src/androidTest/java/com/capstone/hibykes/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/capstone/hibykes/ExampleInstrumentedTest.kt
@@ -11,4 +11,10 @@ import org.junit.Assert.*
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * See [testing documentation](http://d.android.com/tools/test
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+  
