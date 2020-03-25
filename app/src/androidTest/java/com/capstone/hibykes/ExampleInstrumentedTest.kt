6505a5414a38f187691332fab9536c308f834262
stubase <107356826+stubase@users.noreply.github.com>
2020-03-25 19:53:12 +0000

    closes #5

--- app/src/androidTest/java/com/capstone/hibykes/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/capstone/hibykes/ExampleInstrumentedTest.kt
@@ -19,4 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        asse
+        assertEquals("com.capstone.hibykes", appContext.packageName)
+    }
+}
