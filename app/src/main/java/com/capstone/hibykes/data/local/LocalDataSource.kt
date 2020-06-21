68770e745c4e53ac85d811ac098b29393bbaf98c
stubase <107356826+stubase@users.noreply.github.com>
2020-06-21 07:08:00 +0000

    Updating package version

--- app/src/main/java/com/capstone/hibykes/data/local/LocalDataSource.kt
+++ app/src/main/java/com/capstone/hibykes/data/local/LocalDataSource.kt
@@ -2,4 +2,7 @@ package com.capstone.hibykes.data.local
 
 import androidx.lifecycle.LiveData
 import com.capstone.hibykes.data.local.entity.BookmarkEntity
-import com.capstone.hibyk
+import com.capstone.hibykes.data.local.entity.PredictionEntity
+import com.capstone.hibykes.data.local.room.HiBykesDao
+import kotlinx.coroutines.CoroutineScope
+imp
