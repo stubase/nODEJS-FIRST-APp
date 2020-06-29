30500e42a48f6f7c5bd1db825498977db993abd0
stubase <107356826+stubase@users.noreply.github.com>
2020-06-29 12:47:27 +0000

    Pontoon: Update Russian (ru) localization of AMO

--- app/src/main/java/com/capstone/hibykes/data/local/entity/PredictionEntity.kt
+++ app/src/main/java/com/capstone/hibykes/data/local/entity/PredictionEntity.kt
@@ -1,2 +1,9 @@
 package com.capstone.hibykes.data.local.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PredictionEntity (
+    val id: String,
+    val stat
