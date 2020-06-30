5cb7c3dba8fde3acfd8ae6acbeb9b8ddd85bc473
stubase <107356826+stubase@users.noreply.github.com>
2020-06-30 07:57:07 +0000

    Delete dummy.txt

--- app/src/main/java/com/capstone/hibykes/data/local/entity/StationEntity.kt
+++ app/src/main/java/com/capstone/hibykes/data/local/entity/StationEntity.kt
@@ -1,1 +1,8 @@
-package com.capstone.hibykes.data.loc
+package com.capstone.hibykes.data.local.entity
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class StationEntity (
+    val id: String? = 
