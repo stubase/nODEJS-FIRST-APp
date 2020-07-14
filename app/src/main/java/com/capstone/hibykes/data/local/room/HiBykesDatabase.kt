8e1236c48b59941e95b76b0e640662b0d578794a
stubase <107356826+stubase@users.noreply.github.com>
2020-07-14 09:57:13 +0000

    More metadata

--- app/src/main/java/com/capstone/hibykes/data/local/room/HiBykesDatabase.kt
+++ app/src/main/java/com/capstone/hibykes/data/local/room/HiBykesDatabase.kt
@@ -4,4 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-impor
+import com.capstone.hibykes.data.local.entity.BookmarkEntity
+
+@Database(entities = [BookmarkEntity::class], version = 1)
+abstract class HiBykes
