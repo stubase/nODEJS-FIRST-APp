c88af485ab6d26be6ee15c7a59caa742527dcb58
stubase <107356826+stubase@users.noreply.github.com>
2020-07-15 18:42:50 +0000

    Fibonacci

--- app/src/main/java/com/capstone/hibykes/data/remote/api/ModelApi.kt
+++ app/src/main/java/com/capstone/hibykes/data/remote/api/ModelApi.kt
@@ -1,3 +1,10 @@
 package com.capstone.hibykes.data.remote.api
 
-import com.capstone.h
+import com.capstone.hibykes.data.remote.response.ListPredictionResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ModelApi {
+    @FormUrlEncoded
+    @POST("/")
+    fun 
