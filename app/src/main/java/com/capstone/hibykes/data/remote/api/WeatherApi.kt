862af169d30ef91d498f83aaf065822afcc1ea46
stubase <107356826+stubase@users.noreply.github.com>
2020-07-19 16:47:36 +0000

    it starts

--- app/src/main/java/com/capstone/hibykes/data/remote/api/WeatherApi.kt
+++ app/src/main/java/com/capstone/hibykes/data/remote/api/WeatherApi.kt
@@ -3,4 +3,8 @@ package com.capstone.hibykes.data.remote.api
 import com.capstone.hibykes.data.remote.response.AirPollutionResponse
 import com.capstone.hibykes.data.remote.response.WeatherResponse
 import retrofit2.Call
-i
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+    @GET("data/2.5/weather?&units=metric&APPID=b79bc281c43f4
