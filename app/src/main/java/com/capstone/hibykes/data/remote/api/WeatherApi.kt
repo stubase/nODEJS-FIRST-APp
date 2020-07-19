d07785f37bcc464ba18ef2f87dfa1d932cb8654b
stubase <107356826+stubase@users.noreply.github.com>
2020-07-19 15:33:21 +0000

    build: mark package.json as private

--- app/src/main/java/com/capstone/hibykes/data/remote/api/WeatherApi.kt
+++ app/src/main/java/com/capstone/hibykes/data/remote/api/WeatherApi.kt
@@ -1,1 +1,6 @@
-package com.capstone.hibykes.da
+package com.capstone.hibykes.data.remote.api
+
+import com.capstone.hibykes.data.remote.response.AirPollutionResponse
+import com.capstone.hibykes.data.remote.response.WeatherResponse
+import retrofit2.Call
+i
