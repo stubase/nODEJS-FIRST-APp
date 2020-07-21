db830d508b6ab03c069b84d0279c35b4c6afd83c
stubase <107356826+stubase@users.noreply.github.com>
2020-07-21 07:53:17 +0000

    Updated portfolio

--- app/src/main/java/com/capstone/hibykes/data/remote/api/WeatherApi.kt
+++ app/src/main/java/com/capstone/hibykes/data/remote/api/WeatherApi.kt
@@ -7,4 +7,9 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
-    @GET("data/2.5/weather?&units=metric&APPID=b79bc281c43f4
+    @GET("data/2.5/weather?&units=metric&APPID=b79bc281c43f45a69cd831ebc73127d8")
+    fun getWeatherData(
+        @Query("q") cityName: String
+    ): Call<WeatherResponse>
+
+    @GET("data/2.5/air_pollution?&appid=b79bc281c4
