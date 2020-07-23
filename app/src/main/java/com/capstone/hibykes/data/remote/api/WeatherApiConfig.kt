bb060b9ad0341d6f85b0771fe8de7449603f736f
stubase <107356826+stubase@users.noreply.github.com>
2020-07-23 06:34:44 +0000

    Ignore sublime files

--- app/src/main/java/com/capstone/hibykes/data/remote/api/WeatherApiConfig.kt
+++ app/src/main/java/com/capstone/hibykes/data/remote/api/WeatherApiConfig.kt
@@ -1,1 +1,6 @@
-p
+package com.capstone.hibykes.data.remote.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.G
