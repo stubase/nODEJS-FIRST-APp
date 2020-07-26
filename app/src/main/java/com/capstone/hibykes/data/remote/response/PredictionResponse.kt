002532641b43520e8c196534dc5de7fa4fd3bde1
stubase <107356826+stubase@users.noreply.github.com>
2020-07-26 16:29:25 +0000

    ver 0.3

--- app/src/main/java/com/capstone/hibykes/data/remote/response/PredictionResponse.kt
+++ app/src/main/java/com/capstone/hibykes/data/remote/response/PredictionResponse.kt
@@ -1,4 +1,11 @@
 package com.capstone.hibykes.data.remote.response
 
 import android.os.Parcelable
-import com.google.gson.annotat
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PredictionResponse(
+
+    @field:SerializedName("datetime")
+    val datetime: 
