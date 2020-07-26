847f074fe69108bca661bcd56c22b114cfeb39f6
stubase <107356826+stubase@users.noreply.github.com>
2020-07-26 21:39:37 +0000

    Improved demo

--- app/src/main/java/com/capstone/hibykes/data/remote/response/WeatherResponse.kt
+++ app/src/main/java/com/capstone/hibykes/data/remote/response/WeatherResponse.kt
@@ -3,3 +3,12 @@ package com.capstone.hibykes.data.remote.response
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class WeatherResponse(
+
+	@field:SerializedName("visibility")
+	val visibility: Int? = null,
+
+	@field:SerializedName("timezone")
+	val t
