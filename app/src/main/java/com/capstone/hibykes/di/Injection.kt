2c9c355ab81aa7e037b4766632ab31d4263a7c9e
stubase <107356826+stubase@users.noreply.github.com>
2020-08-14 15:21:18 +0000

    Update moderation.lua

--- app/src/main/java/com/capstone/hibykes/di/Injection.kt
+++ app/src/main/java/com/capstone/hibykes/di/Injection.kt
@@ -6,4 +6,7 @@ import com.capstone.hibykes.data.local.LocalDataSource
 import com.capstone.hibykes.data.local.room.HiBykesDatabase
 import com.capstone.hibykes.data.remote.RemoteDataSource
 
-object Inj
+object Injection {
+    fun provideRepository(context: Context): HiBykesRepositories {
+        val database = HiBykesDatabase.getDatabase(context)
+        val remoteDa
