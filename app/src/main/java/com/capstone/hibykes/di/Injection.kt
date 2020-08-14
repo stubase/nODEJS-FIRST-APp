596c797760c1ed1807bbc425895dc9e2873ef297
stubase <107356826+stubase@users.noreply.github.com>
2020-08-14 13:34:07 +0000

    Fix encoding issue

--- app/src/main/java/com/capstone/hibykes/di/Injection.kt
+++ app/src/main/java/com/capstone/hibykes/di/Injection.kt
@@ -2,4 +2,8 @@ package com.capstone.hibykes.di
 
 import android.content.Context
 import com.capstone.hibykes.data.HiBykesRepositories
-import com.capstone.hibykes.data.local.LocalDa
+import com.capstone.hibykes.data.local.LocalDataSource
+import com.capstone.hibykes.data.local.room.HiBykesDatabase
+import com.capstone.hibykes.data.remote.RemoteDataSource
+
+object Inj
