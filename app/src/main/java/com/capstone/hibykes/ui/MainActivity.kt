25bbcb79ad618bddd2380e14201102ba685b75ae
stubase <107356826+stubase@users.noreply.github.com>
2020-08-18 09:42:08 +0000

    Update readme.md file

--- app/src/main/java/com/capstone/hibykes/ui/MainActivity.kt
+++ app/src/main/java/com/capstone/hibykes/ui/MainActivity.kt
@@ -4,4 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.capstone.hibykes.R
-import com.caps
+import com.capstone.hibykes.databinding.ActivityMainBinding
+import com.capstone.hibykes.ui.bookmark.BookmarkFragment
+import com.capstone.hibykes.ui.home.
