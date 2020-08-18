b72e23dd7000dd646c79872b0f73939eed2abaec
stubase <107356826+stubase@users.noreply.github.com>
2020-08-18 18:34:28 +0000

    minifix

--- app/src/main/java/com/capstone/hibykes/ui/MainActivity.kt
+++ app/src/main/java/com/capstone/hibykes/ui/MainActivity.kt
@@ -6,4 +6,9 @@ import androidx.fragment.app.Fragment
 import com.capstone.hibykes.R
 import com.capstone.hibykes.databinding.ActivityMainBinding
 import com.capstone.hibykes.ui.bookmark.BookmarkFragment
-import com.capstone.hibykes.ui.home.
+import com.capstone.hibykes.ui.home.HomeFragment
+import com.capstone.hibykes.ui.maps.MapsFragment
+import com.capstone.hibykes.ui.settings.SettingsFragment
+
+
+class MainA
