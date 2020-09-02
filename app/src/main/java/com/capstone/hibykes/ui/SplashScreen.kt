134d54b35b03016b839eb32ab5b6f8011aa269ab
stubase <107356826+stubase@users.noreply.github.com>
2020-09-02 20:57:00 +0000

    added sample project

--- app/src/main/java/com/capstone/hibykes/ui/SplashScreen.kt
+++ app/src/main/java/com/capstone/hibykes/ui/SplashScreen.kt
@@ -5,4 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
-import com.
+import com.capstone.hibykes.R
+import com.capstone.hibykes.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase
