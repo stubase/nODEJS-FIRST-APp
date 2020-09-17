6feb3ba2d990c54b22facedcf2c463ca6906e4f9
stubase <107356826+stubase@users.noreply.github.com>
2020-09-17 13:41:03 +0000

    fix tabbing

--- app/src/main/java/com/capstone/hibykes/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/capstone/hibykes/ui/bookmark/BookmarkFragment.kt
@@ -23,4 +23,6 @@ class BookmarkFragment : Fragment() {
     private lateinit var bookmarkAdapter: BookmarkAdapter
     private var bookmarks = ArrayList<PredictionEntity>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        fragmentBookmarkBinding = FragmentBookmarkBinding.inflate(layoutInflater, container, false)
+        return fragmentBookmar
