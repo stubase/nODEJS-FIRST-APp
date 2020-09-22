b122065d11653568afceb60aec6a6f8769a07058
stubase <107356826+stubase@users.noreply.github.com>
2020-09-22 10:30:59 +0000

    Few fixes

--- app/src/main/java/com/capstone/hibykes/ui/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/capstone/hibykes/ui/bookmark/BookmarkViewModel.kt
@@ -3,4 +3,7 @@ package com.capstone.hibykes.ui.bookmark
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.capstone.hibykes.data.HiBykesRepositories
-import com.capstone.hibykes.data.local.entity.Bookma
+import com.capstone.hibykes.data.local.entity.BookmarkEntity
+
+class BookmarkViewModel(private val hiBykesRepositories: HiBykesRepositories) : ViewModel() {
+    fun getAllBookmark(): LiveData<List<Bo
