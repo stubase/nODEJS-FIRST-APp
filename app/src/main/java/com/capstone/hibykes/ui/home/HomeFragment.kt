4b31c11ea516ba2eb3fb116cad4e4e7e6c728968
stubase <107356826+stubase@users.noreply.github.com>
2020-09-23 17:22:12 +0000

    USB: Fix persist resume of some SS USB devices

--- app/src/main/java/com/capstone/hibykes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/capstone/hibykes/ui/home/HomeFragment.kt
@@ -5,4 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import andro
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayou
