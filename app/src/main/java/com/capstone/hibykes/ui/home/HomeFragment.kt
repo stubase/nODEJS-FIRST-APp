d0c081200e3c369bf5f30a67a4f1f84ff28a5cd3
stubase <107356826+stubase@users.noreply.github.com>
2020-09-23 10:24:54 +0000

    Czech screenshot

--- app/src/main/java/com/capstone/hibykes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/capstone/hibykes/ui/home/HomeFragment.kt
@@ -1,3 +1,8 @@
 package com.capstone.hibykes.ui.home
 
-import android.co
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import andro
