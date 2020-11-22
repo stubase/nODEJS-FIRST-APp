4b16593e65b730a4a81fb269c18a7ed88ff7c326
stubase <107356826+stubase@users.noreply.github.com>
2020-11-22 16:12:06 +0000

    refs #0000

--- app/src/main/java/com/capstone/hibykes/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/capstone/hibykes/ui/home/HomeViewModel.kt
@@ -4,4 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.capstone.hibykes.data.HiBykesRepositories
 import com.capstone.hibykes.data.local.entity.StationEntity
-import com.capstone.hibykes.data.remote.re
+import com.capstone.hibykes.data.remote.response.AirPollutionResponse
+import com.capstone.hibykes.data.remote.response.WeatherResponse
+
+class HomeViewModel(private val hiBykesRepositories: HiBykesRepositories
