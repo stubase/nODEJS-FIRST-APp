0e95de7921cadc9b6537000bfc6e4e341e550153
stubase <107356826+stubase@users.noreply.github.com>
2020-11-27 10:36:09 +0000

    Fixed Russian translation

--- app/src/main/java/com/capstone/hibykes/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/capstone/hibykes/ui/home/HomeViewModel.kt
@@ -9,4 +9,5 @@ import com.capstone.hibykes.data.remote.response.WeatherResponse
 
 class HomeViewModel(private val hiBykesRepositories: HiBykesRepositories) : ViewModel() {
     fun getCurrentWeather(city: String): LiveData<WeatherResponse> = hiBykesRepositories.getCurrentWeather(city)
-    fun getAirPollution(lat: Double
+    fun getAirPollution(lat: Double, lon: Double): LiveData<AirPollutionResponse> = hiBykesRepositories.getAirPollution(lat, lon)
+    fun getStationsData(): LiveData<ArrayList<Station
