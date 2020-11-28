0a3153905cea28d6731fc75e444b19f055bb6467
stubase <107356826+stubase@users.noreply.github.com>
2020-11-28 14:18:09 +0000

    CVS_SILENT i18n style guide fix

--- app/src/main/java/com/capstone/hibykes/ui/home/StationAdapter.kt
+++ app/src/main/java/com/capstone/hibykes/ui/home/StationAdapter.kt
@@ -4,4 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.Reque
+import com.bumptech.glide.request.RequestOptions
+import com.capstone.hibykes.R
+import com.capstone.hibykes.data.local.entity.StationEntity
+import com.capstone.hibykes.databinding.I
