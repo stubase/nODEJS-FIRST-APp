4c940968018795aa441174c4b8a1249bf0e38a9e
stubase <107356826+stubase@users.noreply.github.com>
2021-01-17 14:18:01 +0000

    output

--- app/src/main/java/com/capstone/hibykes/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/hibykes/ui/login/LoginActivity.kt
@@ -1,4 +1,9 @@
 package com.capstone.hibykes.ui.login
 
 import android.app.Dialog
-import android.conte
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+impo
