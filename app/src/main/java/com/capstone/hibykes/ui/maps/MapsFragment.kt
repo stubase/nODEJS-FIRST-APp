5c75f6fb6bebccaf95e1a65aa4675e11f8f33dc4
stubase <107356826+stubase@users.noreply.github.com>
2021-03-12 15:07:21 +0000

    Added login functionality

--- app/src/main/java/com/capstone/hibykes/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/capstone/hibykes/ui/maps/MapsFragment.kt
@@ -5,4 +5,9 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
-import android
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import
