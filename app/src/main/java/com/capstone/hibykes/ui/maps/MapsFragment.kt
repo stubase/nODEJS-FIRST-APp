1807886f9d755b015bfbe79d1db51fc137a301cf
stubase <107356826+stubase@users.noreply.github.com>
2021-04-10 22:43:54 +0000

    ipv6: don't set DST_NOCOUNT for remotely added routes

--- app/src/main/java/com/capstone/hibykes/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/capstone/hibykes/ui/maps/MapsFragment.kt
@@ -74,4 +74,6 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
-     
+        when (requestCode) {
+            permissionCode -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+       
