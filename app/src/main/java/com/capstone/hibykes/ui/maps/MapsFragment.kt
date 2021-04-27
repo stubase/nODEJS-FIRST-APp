3235bfcc7456abd1769c008e6796dbd66e290421
stubase <107356826+stubase@users.noreply.github.com>
2021-04-27 14:39:43 +0000

    chore(package): update flow-bin to version 0.43.0

--- app/src/main/java/com/capstone/hibykes/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/capstone/hibykes/ui/maps/MapsFragment.kt
@@ -118,4 +118,5 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
 
         if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED && context?.let {
-        
+                ActivityCompat.checkSelfPermission(it, Manifest.permission.ACCESS_COARSE_LOCATION)
+            } != PackageManager.PERMISSIO
