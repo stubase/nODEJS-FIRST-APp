05e57e2952bba06dde77f056318806aad7309c99
stubase <107356826+stubase@users.noreply.github.com>
2021-05-08 05:39:57 +0000

    Updated JS

--- app/src/main/java/com/capstone/hibykes/ui/prediction/PredictionViewModel.kt
+++ app/src/main/java/com/capstone/hibykes/ui/prediction/PredictionViewModel.kt
@@ -4,4 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.capstone.hibykes.data.HiBykesRepositories
 import com.capstone.hibykes.data.local.entity.PredictionEntity
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.co
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class PredictionViewModel(private val hiBykesRepositories: HiBykesRepositories) : ViewModel() 
