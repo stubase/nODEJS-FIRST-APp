20866de0214cd60e27a38784cb0afdf9764ba011
stubase <107356826+stubase@users.noreply.github.com>
2021-05-10 19:29:22 +0000

    pf

--- app/src/main/java/com/capstone/hibykes/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/capstone/hibykes/ui/register/RegisterActivity.kt
@@ -7,4 +7,8 @@ import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-i
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.a
