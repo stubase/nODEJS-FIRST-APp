bb9ad5f911d34e1ca83ab46ad1cfeb1e3e040c69
stubase <107356826+stubase@users.noreply.github.com>
2021-05-10 18:47:31 +0000

    Italian translation

--- app/src/main/java/com/capstone/hibykes/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/capstone/hibykes/ui/register/RegisterActivity.kt
@@ -3,4 +3,8 @@ package com.capstone.hibykes.ui.register
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
-import andro
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+i
