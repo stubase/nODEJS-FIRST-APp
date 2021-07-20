befbe4fa70faf49ed28f28ce4127178aef87d954
stubase <107356826+stubase@users.noreply.github.com>
2021-07-20 08:45:15 +0000

    Missed a bit

--- app/src/main/java/com/capstone/hibykes/ui/station/PredictionAdapter.kt
+++ app/src/main/java/com/capstone/hibykes/ui/station/PredictionAdapter.kt
@@ -2,3 +2,6 @@ package com.capstone.hibykes.ui.station
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.capstone.hibykes.data.local.entity.PredictionEntity
+import com.capstone.hibykes.databi
