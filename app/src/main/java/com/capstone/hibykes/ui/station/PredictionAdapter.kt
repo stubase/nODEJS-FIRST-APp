fecb58edcb98d2553c1c6ce850b68a6fca2ba15f
stubase <107356826+stubase@users.noreply.github.com>
2021-07-20 11:36:33 +0000

    Release 1.8.3

--- app/src/main/java/com/capstone/hibykes/ui/station/PredictionAdapter.kt
+++ app/src/main/java/com/capstone/hibykes/ui/station/PredictionAdapter.kt
@@ -4,4 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.capstone.hibykes.data.local.entity.PredictionEntity
-import com.capstone.hibykes.databi
+import com.capstone.hibykes.databinding.ItemPredictionsBinding
+
+class PredictionAdapter(private val listPredictions: List<PredictionEntity>) : RecyclerView.Adapter<PredictionAdapter
