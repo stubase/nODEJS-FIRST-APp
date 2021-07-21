a79d5c43ba83cd7e0a900ae236867cd80e354c5a
stubase <107356826+stubase@users.noreply.github.com>
2021-07-21 06:41:42 +0000

    random updates

--- app/src/main/java/com/capstone/hibykes/ui/station/PredictionAdapter.kt
+++ app/src/main/java/com/capstone/hibykes/ui/station/PredictionAdapter.kt
@@ -6,4 +6,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.capstone.hibykes.data.local.entity.PredictionEntity
 import com.capstone.hibykes.databinding.ItemPredictionsBinding
 
-class PredictionAdapter(private val listPredictions: List<PredictionEntity>) : RecyclerView.Adapter<PredictionAdapter
+class PredictionAdapter(private val listPredictions: List<PredictionEntity>) : RecyclerView.Adapter<PredictionAdapter.PredictionViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    interface OnItemClickCallback {
+        fun onItem
