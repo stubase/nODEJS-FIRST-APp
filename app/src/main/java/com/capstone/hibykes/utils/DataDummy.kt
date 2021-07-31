9e3c43c915a7565fdbe241b333c35bb094b50bfc
stubase <107356826+stubase@users.noreply.github.com>
2021-07-31 16:01:55 +0000

    jump

--- app/src/main/java/com/capstone/hibykes/utils/DataDummy.kt
+++ app/src/main/java/com/capstone/hibykes/utils/DataDummy.kt
@@ -1,3 +1,8 @@
 package com.capstone.hibykes.utils
 
-import com.capstone.
+import com.capstone.hibykes.data.local.entity.PredictionEntity
+import com.capstone.hibykes.data.local.entity.StationEntity
+
+object DataDummy {
+
+    fun generateData
